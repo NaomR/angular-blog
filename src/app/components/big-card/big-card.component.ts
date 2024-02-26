7f6c9bccf90c03b85b5a2f0d6b0295dd4ee67485
Naom Ricardo <78932401+NaomR@users.noreply.github.com>
2024-02-26 14:37:23 -0300

    alpha Finalizado

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
-
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardAuthor:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:String= ""
+  @Input()
+  Id:string='0'
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    'id':"1",
+    'photo':"https://s2-g1.glbimg.com/milhLAZBJNyppqQFnbBRjiIVlEA=/0x0:1024x683/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/G/1/bVZDj5RWmMv9UCnf0DpQ/b73d5fa0-145f-43a2-a434-5a9041163f26.jpg",
+    title:"Cine Líbero Luxardo exibe filmes em língua francesa no 'Ciclo de Cinema Internacional'",
+    description:"Programação é entre os dias 17 e 23 de março e a entrada é franca. Cinema também terá sessões regulares."
+  },
+  {
+    'id':"2",
+    'photo':"https://inteligenciafinanceira.com.br/wp-content/uploads/2021/12/IF-Futebol.png",
+    title:"A Evolução do Futebol das Américas",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu elementum tellus, eget laoreet est. Sed nisl nibh, luctus sit amet lacus a, cursus sollicitudin velit. Cras ut tellus libero. Donec sed mauris luctus, suscipit sapien ac, mollis quam. Phasellus rutrum arcu at ipsum euismod dapibus. Donec suscipit ligula et metus efficitur, ac condimentum urna lobortis. Aenean quam orci, bibendum in mollis vel, varius nec sem. Curabitur ut dolor porttitor, commodo augue at, semper velit. Phasellus nulla velit, cursus id gravida vel, fringilla et ligula."
+  },
+  {
+    'id':"3",
+    'photo':"https://www.8milimetros.com.br/wp-content/uploads/2019/07/esports.jpeg",
+    title:"A Força dos eSports no mundo",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu elementum tellus, eget laoreet est. Sed nisl nibh, luctus sit amet lacus a, cursus sollicitudin velit. Cras ut tellus libero. Donec sed mauris luctus, suscipit sapien ac, mollis quam. Phasellus rutrum arcu at ipsum euismod dapibus. Donec suscipit ligula et metus efficitur, ac condimentum urna lobortis. Aenean quam orci, bibendum in mollis vel, varius nec sem. Curabitur ut dolor porttitor, commodo augue at, semper velit. Phasellus nulla velit, cursus id gravida vel, fringilla et ligula."
+  },
+  {
+    'id':"4",
+    'photo':"https://blog.singularityubrazil.com/wp-content/uploads/2023/03/nanotecnologia-eletronica.jpg",
+    title:"nanotecnologia:A tecnologia a nivel microscópico",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu elementum tellus, eget laoreet est. Sed nisl nibh, luctus sit amet lacus a, cursus sollicitudin velit. Cras ut tellus libero. Donec sed mauris luctus, suscipit sapien ac, mollis quam. Phasellus rutrum arcu at ipsum euismod dapibus. Donec suscipit ligula et metus efficitur, ac condimentum urna lobortis. Aenean quam orci, bibendum in mollis vel, varius nec sem. Curabitur ut dolor porttitor, commodo augue at, semper velit. Phasellus nulla velit, cursus id gravida vel, fringilla et ligula."
+  }
+]
